8e16bd10153c380348813553fe57e8eb0621c265
peninah-njeri <peninahgathuru@gmail.com>
2019-10-14 12:15:52 +0300

    get data from my api

--- app/request.py
+++ app/request.py
@@ -3,7 +3,3 @@ import urllib.request,json
 from .Models import source
 from .Models import article
 
-
-
-# Getting api key
-api_key = app.config['NEWS_API_KEY']
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from.request import get_sources
 # Views
 @app.route('/')
 def index():
@@ -8,6 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    # Getting specific source
+    specific_sources=get_sources('specific')
 
     title = 'Home - Welcome to The best News Website Online'
-    return render_template('index.html', title =title)
+    return render_template('index.html', title = title,specific= specific_sources)
